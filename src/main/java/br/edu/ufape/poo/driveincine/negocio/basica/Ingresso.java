6527014e8594b1103d944c8771c275202ad3f5ab
RafaelaFoerster <rafafoerst@gmail.com>
2023-08-04 15:09:56 -0300

    Enviando classe Ingresso

---
+++ src/main/java/br/edu/ufape/poo/driveincine/negocio/basica/Ingresso.java
@@ -0,0 +1,10 @@
+package br.edu.ufape.poo.driveincine.negocio.basica;
+
+public class Ingresso {
+	private String qrcode;
+	private String dataEmissao;
+	private int quantidade;
+	private String cidade;
+	private String diaExibicao;
+	private double preço;
+}
