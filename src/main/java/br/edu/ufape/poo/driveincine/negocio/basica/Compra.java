5ecead96dd5d43dd486c6b7cd9e67e93e3da3bf2
RafaelaFoerster <rafafoerst@gmail.com>
2023-08-14 16:14:30 -0300

    Colocando o construtor da Classe Compra

--- src/main/java/br/edu/ufape/poo/driveincine/negocio/basica/Compra.java
+++ src/main/java/br/edu/ufape/poo/driveincine/negocio/basica/Compra.java
@@ -40,4 +40,10 @@ public class Compra {
 	public void setStatus(String status) {
 		this.status = status;
 	}
+	public Compra(float valorTotal, Date dataEmissao, String status) {
+		super();
+		this.valorTotal = valorTotal;
+		this.dataEmissao = dataEmissao;
+		this.status = status;
+	}
 }
